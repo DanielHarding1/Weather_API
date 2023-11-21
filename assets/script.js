3de66c7d7ee77730c6a922e30c856f04fcb0b96f
Daniel Harding <danielharding2008@gmail.com>
2023-11-21 22:59:11 +0000

    Forecast logic complete, styling needed

--- assets/script.js
+++ assets/script.js
@@ -24,14 +24,65 @@ function displayWeatherInfo() {
       console.log(data);
       var locationTitle = $("<h1>").text(data.name);
       $(".current-forecast").prepend(locationTitle);
-      var locationTemp = $("<p>").text("Temperature: " + data.main.temp);
+      var Celsius = (data.main.temp - 273.15).toFixed(1);
+      var locationTemp = $("<p>").text("Temperature: " + Celsius + " C");
       $(".current-forecast").append(locationTemp);
+      var locationHum = $("<p>").text("Humidity: " + data.main.humidity + "%");
+      $(".current-forecast").append(locationHum);
+      var locationWind = $("<p>").text("Wind: " + data.wind.speed + " KPH");
+      $(".current-forecast").append(locationWind);
       var iconCode = data.weather[0].icon;
       var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
       var weatherIcon = $("<img>")
         .attr("src", iconURL)
         .attr("alt", "Weather Icon");
       $(".current-forecast").append(weatherIcon);
+
+      var lat = data.coord.lat;
+      var long = data.coord.lon;
+
+      var forecastURL =
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        long +
+        "&appid=d3f1d668ff3a88cdb44b7f7575041175";
+      fetch(forecastURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data2) {
+          console.log(forecastURL);
+          console.log(data2);
+          $("#forecast-weather").empty();
+          for (var i = 12; i < 60; i = i + 8) {
+            var forecastTitle = $("<card>")
+              .addClass("container")
+              .text(data2.list[i].dt_txt);
+            $("#forecast-weather").append(forecastTitle);
+            var iconCode = data2.list[i].weather[0].icon;
+            var iconURL =
+              "http://openweathermap.org/img/w/" + iconCode + ".png";
+            var weatherIcon = $("<img>")
+              .addClass("container")
+              .attr("src", iconURL)
+              .attr("alt", "Weather Icon");
+            $(forecastTitle).append(weatherIcon);
+            var Celsius = (data2.list[i].main.temp - 273.15).toFixed(1);
+            var forecastTemp = $("<p>")
+              .addClass("container")
+              .text("Temperature: " + Celsius + " C");
+            $(forecastTitle).append(forecastTemp);
+            var forecastHum = $("<p>")
+              .addClass("container")
+              .text("Humidity: " + data2.list[i].main.humidity + "%");
+            $(forecastTitle).append(forecastHum);
+            var forecastWind = $("<p>")
+              .addClass("container")
+              .text("Wind: " + data2.list[i].wind.speed + " KPH");
+            $(forecastTitle).append(forecastWind);
+          }
+        });
     });
 }
 
